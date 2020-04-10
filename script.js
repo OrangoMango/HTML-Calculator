d9a6215ea881efc3edb90ba753d29f386f1e3ec9
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-04-10 19:29:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function insert(num){
+        document.form.textinput.value = document.form.textinput.value + num;
+      }
+      
+      function clean() {
+        // clean input area
+        document.form.textinput.value = "";
+      }
+      
+      function back() {
+        var text = document.form.textinput.value
+       // remove last character from string
+        document.form.textinput.value = text.substring(0,text.length-1)
+      }
+      
+      function calculate(){
+        // eval calculation
+        var text = document.form.textinput.value
+        document.form.textinput.value = eval(text)
+      }
