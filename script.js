de324e13aa1a4111ec10d9e0927e5fdf5bebfe39
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-04-10 19:35:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function insert(num){
         document.form.textinput.value = document.form.textinput.value + num;
       }
       
-      function clean() {
+function clean() {
         // clean input area
         document.form.textinput.value = "";
       }
       
-      function back() {
+function back() {
         var text = document.form.textinput.value
        // remove last character from string
         document.form.textinput.value = text.substring(0,text.length-1)
       }
-      
-      function calculate(){
+ 
+function calculate(){
         // eval calculation
         var text = document.form.textinput.value
         document.form.textinput.value = eval(text)
